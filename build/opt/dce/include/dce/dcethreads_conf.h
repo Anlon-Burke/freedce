df9a4749ffcd7728169c0f43969781d82039d0ee
Anlon-Burke <Anlon-Burke@users.noreply.github.com>
2019-12-08 02:07:02 +0100

    changed versions

--- build/opt/dce/include/dce/dcethreads_conf.h
+++ build/opt/dce/include/dce/dcethreads_conf.h
@@ -87,13 +87,13 @@
 #define PACKAGE_NAME "dcethreads"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "dcethreads 1.0"
+#define PACKAGE_STRING "dcethreads 2.0.2"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "dcethreads"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "1.0"
+#define PACKAGE_VERSION "2.0.2"
 
 /* Define to 1 if you have the ANSI C header files. */
 #define STDC_HEADERS 1
@@ -102,7 +102,7 @@
 #define TIME_WITH_SYS_TIME 1
 
 /* Version number of package */
-#define VERSION "1.0"
+#define VERSION "2.0.2"
 
 /* Define to empty if `const' does not conform to ANSI C. */
 /* #undef const */
